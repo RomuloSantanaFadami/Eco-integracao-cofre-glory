304249a7fa81ac10e6562c227fa10766c3e66b68
diegoFadami <62075327+diegoFadami@users.noreply.github.com>
2022-02-07 18:31:59 -0300

    inicio de tudo

---
+++ IntegracaoCofre/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IntegracaoCofre.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ IntegracaoCofre/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntegracaoCofre.WebApiApplication" Language="C#" %>
