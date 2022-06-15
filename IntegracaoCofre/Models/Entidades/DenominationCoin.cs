e328c2db077b6df0d748f97e911c1abc95a2cca6
diegoFadami <62075327+diegoFadami@users.noreply.github.com>
2022-06-15 17:35:27 -0300

    .

---
+++ IntegracaoCofre/Models/Entidades/DenominationCoin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntegracaoCofre.Models
+{
+    public class DenominationCoin
+    {
+        public int Amount { get; set; }
+        public string Value { get; set; }
+    }
+}
--- IntegracaoCofre/Models/RequestRegistrarTransacao.cs
+++ IntegracaoCofre/Models/RequestRegistrarTransacao.cs
@@ -7,6 +7,7 @@ namespace IntegracaoCofre.Models
 {
     public class RequestRegistrarTransacao
     {
+        public string view { get; set; }
         public int TransactionType { get; set; }
         public string SerialNumber { get; set; }
         public string ClientName { get; set; }
@@ -21,6 +22,7 @@ namespace IntegracaoCofre.Models
         public string Currency { get; set; }
         public decimal Amount { get; set; }
         public List<Denomination> ListDenomination { get; set; }
+        public List<DenominationCoin> ListDenominationCoin { get; set; }
         public int RecognizedBills { get; set; }
         public DateTime BusDate { get; set; }
         public int ChangeTransactionId { get; set; }
